eeb0cf612525e515473a917377a96309e1a3c55b
sahid <sahid@acception.com.br>
2018-06-27 16:56:11 -0300

    Initial commit a Social Bookmarking Application with Django

---
+++ django_bookmarks/urls.py
@@ -0,0 +1,9 @@
+from django.conf.urls.defaults import *
+from bookmarks.views import *
+
+# Uncomment the next two lines to enable the admin:
+# from django.contrib import admin
+# admin.autodiscover()
+
+urlpatterns = patterns('', (r'^$', main_page), (r'^user/(\w+)/$', user_page),)
+
