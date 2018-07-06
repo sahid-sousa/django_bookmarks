b2d4ad55cd8638446bcb964a3c78b320e1d40769
sahid <sahid@acception.com.br>
2018-07-06 12:50:56 -0300

    Chapter 4

--- django_bookmarks/bookmarks/views.py
+++ django_bookmarks/bookmarks/views.py
@@ -1,19 +1,17 @@
 # Create your views here.
-from django.http import HttpResponse, Http404
+from django.http import HttpResponse, Http404, HttpResponseRedirect
 from django.contrib.auth.models import User
+from django.contrib.auth import logout
 from django.template import Context
 from django.template.loader import get_template
+from django.shortcuts import render_to_response
 
 
 def main_page(request):
-    template = get_template('main_page.html')
-    variables = Context({
-        'head_title': 'Django Bookmarks',
-        'page_title': 'Welcome to Django Bookmarks',
-        'page_body': 'Where you can store share bookmark'
-    })
-    output = template.render(variables)
-    return HttpResponse(output)
+    return render_to_response(
+        'main_page.html',
+        {'user': request.user}
+    )
 
 
 def user_page(request, username):
@@ -29,3 +27,8 @@ def user_page(request, username):
     })
     output = template.render(variables)
     return HttpResponse(output)
+
+
+def logout_page(request):
+    logout(request)
+    return HttpResponseRedirect('/')
--- django_bookmarks/urls.py
+++ django_bookmarks/urls.py
@@ -5,5 +5,9 @@ from bookmarks.views import *
 # from django.contrib import admin
 # admin.autodiscover()
 
-urlpatterns = patterns('', (r'^$', main_page), (r'^user/(\w+)/$', user_page),)
+urlpatterns = patterns('',
+                       (r'^$', main_page),
+                       (r'^user/(\w+)/$', user_page),
+                       (r'^login/$', 'django.contrib.auth.views.login'),
+                       (r'^logout/$', logout_page),)
 
